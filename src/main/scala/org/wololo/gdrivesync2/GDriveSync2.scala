0917630c4207d17e6b56322ebcccd9596364cbee
Björn Harrtell <bjorn@wololo.org>
2014-05-25 00:19:29 +0200

    Organize imports

--- src/main/scala/org/wololo/gdrivesync2/GDriveSync2.scala
+++ src/main/scala/org/wololo/gdrivesync2/GDriveSync2.scala
@@ -1,24 +1,22 @@
 package org.wololo.gdrivesync2
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
-import com.google.api.client.http.HttpTransport
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
-import com.google.api.client.json.JsonFactory
-import com.google.api.client.json.jackson2.JacksonFactory
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
-import java.util.Collections
-import com.google.api.services.drive.DriveScopes
-import com.google.api.client.util.store.FileDataStoreFactory
+import java.io.InputStreamReader
+
+import scala.collection.JavaConversions.asScalaBuffer
+import scala.collection.JavaConversions.seqAsJavaList
+import scala.collection.mutable.ListBuffer
+
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
-import java.io.InputStreamReader
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.util.store.FileDataStoreFactory
 import com.google.api.services.drive.Drive
-import java.util.ArrayList
+import com.google.api.services.drive.DriveScopes
 import com.google.api.services.drive.model.File
-import scala.collection.mutable.ListBuffer
-import scala.collection.JavaConversions._
 import com.typesafe.scalalogging.slf4j.LazyLogging
-import com.google.api.services.drive.model.User
 
 object GDriveSync2 extends App with LazyLogging {
   val DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"), ".gdrivesync2")
@@ -35,8 +33,7 @@ object GDriveSync2 extends App with LazyLogging {
 
   val credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver())
   	.authorize("user")
-  
-  	logger.debug("Authorized with Google Drive: " + credential.getAccessToken())
+  logger.debug("Authorized with Google Drive: " + credential.getAccessToken())
 
   val builder = new Drive.Builder(httpTransport, JSON_FACTORY, credential)
   builder.setApplicationName("GDriveSync")
