0c5bd51b5e9bb7c540dd1261abd6dded32b2bec8
Björn Harrtell <bjorn@wololo.org>
2014-05-25 00:16:12 +0200

    Finished implementation to fetch and parse remote metadata

--- src/main/scala/org/wololo/gdrivesync2/GDriveSync2.scala
+++ src/main/scala/org/wololo/gdrivesync2/GDriveSync2.scala
@@ -15,39 +15,84 @@ import java.io.InputStreamReader
 import com.google.api.services.drive.Drive
 import java.util.ArrayList
 import com.google.api.services.drive.model.File
+import scala.collection.mutable.ListBuffer
+import scala.collection.JavaConversions._
+import com.typesafe.scalalogging.slf4j.LazyLogging
+import com.google.api.services.drive.model.User
 
-object GDriveSync2 extends App {
+object GDriveSync2 extends App with LazyLogging {
   val DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"), ".gdrivesync2")
 
   val httpTransport = GoogleNetHttpTransport.newTrustedTransport
   val JSON_FACTORY = JacksonFactory.getDefaultInstance
   val dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
 
-  val clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(GDriveSync2.getClass().getResourceAsStream("/client_secrets.json")))
-  val flow = new GoogleAuthorizationCodeFlow.Builder(
-    httpTransport, JSON_FACTORY, clientSecrets,
-    Collections.singleton(DriveScopes.DRIVE)).setDataStoreFactory(
-    dataStoreFactory).build();
+  val clientSecretsStream = GDriveSync2.getClass.getResourceAsStream("/client_secrets.json")
+  val clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(clientSecretsStream))
+  val flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, clientSecrets, List(DriveScopes.DRIVE))
+  	.setDataStoreFactory(dataStoreFactory)
+  	.build
 
-  val credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user")
+  val credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver())
+  	.authorize("user")
+  
+  	logger.debug("Authorized with Google Drive: " + credential.getAccessToken())
 
   val builder = new Drive.Builder(httpTransport, JSON_FACTORY, credential)
-  builder.setApplicationName("GDriveSync");
-  val drive = builder.build()
+  builder.setApplicationName("GDriveSync")
+  val drive = builder.build
 
-  val request = drive.files().list()
-  val result = new ArrayList[File]()
-  
-  // TODO: use scala collections? 
+  val driveRootId = drive.about.get.execute.getRootFolderId
+  val driveRoot = new File
+  driveRoot.setId(driveRootId)
+  val root = new SyncFile("", driveRoot)
+  logger.debug("Found root: " + driveRootId)
 
+  val request = drive.files.list
+  request.setMaxResults(200)
+  val result = ListBuffer[File]()
   do {
-    val files = request.execute()
-    result.addAll(files.getItems())
-  } while (request.getPageToken() != null && request.getPageToken().length() > 0)
+    val files = request.execute
+    val items = files.getItems
+    logger.debug("Fetched items: " + items.length)
+    result ++= items
+    request.setPageToken(files.getNextPageToken)
+  } while (request.getPageToken != null && request.getPageToken.length > 0)
 
-  // TODO: parse into SyncFiles 
-  
-  // TODO: implement sync
-    
-  println(result.get(0).getTitle())
+  logger.debug("Fetched " + result.size + " from Google Drive API")
+
+  var notOwned = result filter { _.getOwners.toList exists { !_.getIsAuthenticatedUser } }
+  logger.debug("Found " + notOwned.size + " items not owned by you")
+  logger.debug("Ignoring items not owned by you")
+  result --= notOwned
+
+  var trashed = result filter { _.getExplicitlyTrashed() != null }
+  logger.debug("Found " + trashed.size + " trashed items")
+  logger.debug("Ignoring trashed items")
+  result --= trashed
+
+  var multipleParents = result filter {_.getParents.length > 1 }
+  logger.debug("Found " + multipleParents.size + " items with multiple parents")
+  logger.debug("Ignoring items with multiple parents")
+  result --= multipleParents
+
+  var noParents = result filter { _.getParents.length == 0 }
+  logger.debug(noParents.size + " items with no parents")
+  logger.debug("Ignoring items with no parents")
+  result --= noParents
+
+  def findChildren(folder: SyncFile) {
+    logger.debug("Searching for children path: " + folder.path)
+    folder.children ++= result.filter(_.getParents().get(0).getId() == folder.driveFile.getId()).map(file => {
+      val isFolder = file.getMimeType == "application/vnd.google-apps.folder"
+      val path = folder.path + file.getTitle + (if (isFolder) "/" else "")
+      logger.debug("Found child with path: " + path)
+      val syncFile = new SyncFile(path, file)
+      if (isFolder) findChildren(syncFile)
+      syncFile
+    })
+  }
+  findChildren(root)
+
+  // TODO: implement local metastore, local filesystem meta recurse, sync
 }
--- src/main/scala/org/wololo/gdrivesync2/SyncFile.scala
+++ src/main/scala/org/wololo/gdrivesync2/SyncFile.scala
@@ -3,11 +3,14 @@ package org.wololo.gdrivesync2
 import com.google.api.services.drive.model.File
 import org.apache.commons.codec.digest.DigestUtils
 import java.io.FileInputStream
+import scala.collection.mutable.ListBuffer
 
-class SyncFile(val localFile: java.io.File, val driveFile: File) {
-  
+class SyncFile(var path: String, val driveFile: File) {
+
+  val localFile = new java.io.File(path)
+
   def localMD5 = DigestUtils.md5Hex(new FileInputStream(localFile))
   def isIdentical = localMD5 == driveFile.getMd5Checksum()
-  
-  
+
+  val children = ListBuffer[SyncFile]()
 }
