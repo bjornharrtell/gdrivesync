d8c18960636e0224b43acbd36ba244b56a23756e
Björn Harrtell <bjorn@wololo.org>
2014-05-26 22:41:49 +0200

    Use Java 7 mimetype probe Add maven shade plugin

--- src/main/scala/org/wololo/gdrivesync2/LocalMetaFetcher.scala
+++ src/main/scala/org/wololo/gdrivesync2/LocalMetaFetcher.scala
@@ -1,22 +1,19 @@
 package org.wololo.gdrivesync2
 
-import org.apache.tika.config.TikaConfig
-import org.apache.tika.io.TikaInputStream
-import org.apache.tika.metadata.Metadata
-import org.apache.tika.metadata.TikaMetadataKeys
-
 import com.google.api.services.drive.model.File
 import com.typesafe.scalalogging.slf4j.LazyLogging
+import java.nio.file.Path
+import java.nio.file.Files
 
 object LocalMetaFetcher extends LazyLogging {
-  def detectMimeType(file: java.io.File) = {
+  /*def detectMimeType(file: java.io.File) = {
     val tika = new TikaConfig()
     val metadata = new Metadata()
     metadata.set(TikaMetadataKeys.RESOURCE_NAME_KEY, file.getName())
     val mimetype = tika.getDetector.detect(TikaInputStream.get(file), metadata)
     logger.debug("Detected mimetype: " + mimetype)
     mimetype.getType()
-  }
+  }*/
 
   def findOrCreate(folder: SyncFile, file: java.io.File) = {
     folder.childAtPath(file) match {
@@ -29,7 +26,7 @@ object LocalMetaFetcher extends LazyLogging {
         if (file.isDirectory()) {
           driveFile.setMimeType("application/vnd.google-apps.folder");
         } else {
-          driveFile.setMimeType(detectMimeType(file));
+          driveFile.setMimeType(Files.probeContentType(file.toPath()));
         }
         val syncFile = new SyncFile(file, driveFile)
         folder.children += syncFile
