bfdcd9db3cef06aab84220da576d7fbd2f8d4d10
Björn Harrtell <bjorn@wololo.org>
2014-05-29 21:11:21 +0200

    Quiet option README updates

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-gdrivesync2
-===========
+# gdrivesync
+
+Command line client written in Scala to sync Google Drive contents to a local folder.
+
+Run with `java -jar gdrivesync-1.x.x.jar`.
+
+```
+    Usage: gdrivesync [OPTIONS]
+
+    OPTIONS
+
+    -c       Clear metadata store
+
+    -d PATH  Directory to represent root of Google Drive when syncing (defaults to
+             ~/gdrive)
+
+    --help   Show usage
+
+    -q       Produce no output
+```
--- src/main/scala/org/wololo/gdrivesync/GDriveSync.scala
+++ src/main/scala/org/wololo/gdrivesync/GDriveSync.scala
@@ -9,27 +9,46 @@ import Globals.httpTransport
 import Globals.DATA_STORE_DIR
 import org.mapdb.DBMaker
 import org.clapper.argot._
+import org.slf4j.LoggerFactory
+import ch.qos.logback.classic.Level
+import org.slf4j.Logger
+
 object GDriveSync extends App with LazyLogging {
 
   implicit val localMetaStore = new LocalMetaStore()
 
   import ArgotConverters._
-  val parser = new ArgotParser("GDriveSync")
+  val parser = new ArgotParser("gdrivesync")
   val dest = parser.option[String](List("d"), "PATH", "Directory to represent root of Google Drive when syncing (defaults to ~/gdrive)")
+  val quiet = parser.flag[Boolean](List("q"), "Produce no output")
   val clear = parser.flag[Boolean](List("c"), "Clear metadata store")
   val help = parser.flag[Boolean](List("help"), "Show usage")
   
   try {
     parser.parse(args)
-    if (help.value.isDefined) parser.usage
+    if (help.value.isDefined) {
+      parser.usage
+      System.exit(0)
+    }
     if (clear.value.isDefined) {
       logger.info("Clearing metadata")
       localMetaStore.clear
       System.exit(0)
     }
-    if (dest.value.isDefined) Globals.SYNC_STORE_DIR = new java.io.File(dest.value.get)
+    if (quiet.value.isDefined) {
+        LoggerFactory
+        	.getLogger(Logger.ROOT_LOGGER_NAME)
+        	.asInstanceOf[ch.qos.logback.classic.Logger]
+            .setLevel(Level.OFF)
+    }
+    if (dest.value.isDefined) {
+      Globals.SYNC_STORE_DIR = new java.io.File(dest.value.get)
+    }
   } catch {
-    case e: ArgotUsageException => println(e.message)
+    case e: ArgotUsageException => {
+      println(e.message)
+      System.exit(0)
+    }
   }
 
   implicit val drive = new Drive.Builder(httpTransport, JSON_FACTORY, GoogleOAuth2.authorize)
